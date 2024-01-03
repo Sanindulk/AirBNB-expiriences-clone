54882f5a5920bb9dd627dcf78f03c53fc8627456
Sanindu Lakshan Kurukulasuriya <100991130+Sanindulk@users.noreply.github.com>
2024-01-03 22:59:16 +0530

    NavBar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../App.css"
+
+export default function Navbar(){
+    return(
+        <nav className="nav-bar">
+        <img src="../src/assets/airbnb-logo.svg" alt="AirBNB Logo" />
+        </nav>
+    )
+}
